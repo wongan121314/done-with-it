2454c745e63c31058b3e24093bd0034c668f5899
wongani wosman <ebs23-wwosman@mubas.ac.mw>
2025-11-20 08:26:58 +0200

    change th serverpagekite url to use http not https

--- done-with-it/src/resources/strings.js
+++ done-with-it/src/resources/strings.js
@@ -6,7 +6,7 @@ const Strings = {
   status_reserved: "Reserved",
   price: "Price",
     report_url: "localhost:5001",
-    backend_url: "https://sampleweb1213.pagekite.me"
+    backend_url: "http://sampleweb1213.pagekite.me"
 };
 
 export default Strings;
